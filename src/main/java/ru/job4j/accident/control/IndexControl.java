9c8d5cde7990768733591c42cf1ca394d69abf1b
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-06-18 17:40:31 +0300

    Загрузка данных в вид

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> name = List.of("Petr", "Vlad", "Ivan");
+        model.addAttribute("users", name);
         return "index";
     }
 }
